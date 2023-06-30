4b336a51334c809f6587585bfe0c627cf189775a
Ebenezr <ebenezarbukosia@gmail.com>
2023-06-30 09:21:38 +0300

    [REF] add navigation link
    add navigation link to return user back to home #resolve5

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,46 @@
-import React from 'react';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import Typography from "@mui/material/Typography";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { Box } from "@mui/material";
+import Link from "next/link";
+import Tooltip from "@mui/material/Tooltip";
 const Navbar = () => {
   return (
-    <Typography
-      variant="h5"
-      gutterBottom
-      component="div"
-      sx={{ p: 2, pb: 0 }}
-      className="font-semibold text-gray-700"
-    >
-      Kanban
-    </Typography>
+    <>
+      <Tooltip title="Home">
+        <Link href="/" className="font-semibold  no-underline ">
+          <Typography
+            variant="h5"
+            gutterBottom
+            component="div"
+            sx={{ p: 2, pb: 0 }}
+            className="font-semibold text-gray-700"
+          >
+            Kanban
+          </Typography>
+        </Link>
+      </Tooltip>
+      <Box className=" flex items-center justify-start">
+        <Typography
+          variant="h6"
+          gutterBottom
+          component="div"
+          sx={{ pl: 2, pb: 0 }}
+          className=" text-gray-600 flex items-center"
+        >
+          Dashboard
+        </Typography>
+        <Typography
+          variant="h6"
+          gutterBottom
+          component="div"
+          sx={{ pl: 2, pb: 0 }}
+          className=" text-gray-500 flex text-sm items-center"
+        >
+          <ChevronRightIcon /> Kanban
+        </Typography>
+      </Box>
+    </>
   );
 };
 
